42d073c3b8b008cbbc69bbd7fe5d369f38d7093f
Shaun Hamilton <shauhami020@gmail.com>
2026-01-06 13:37:49 +0200

    feat: add exam utils

---
+++ exam-utils/src/lib.rs
@@ -0,0 +1,13 @@
+//! Exam Environment Utility Functions
+//!
+//! ## Current API
+//!
+//! - Calculate attempt score
+//! - Validate exam config
+//! - Generate exams
+//! - Validate generated exams
+//!
+pub mod attempt;
+pub mod error;
+pub mod generation;
+pub mod misc;
